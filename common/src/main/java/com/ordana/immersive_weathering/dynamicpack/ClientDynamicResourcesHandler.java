df19024d8b8834000aef856b29c1b1c66145813a
f-raZ0R <MonkeyCityOwner@gmail.com>
2024-11-26 11:47:58 -0500

    help i fucked up my gradle and it won't launch runClient so i can't actually test any of this anymore

--- common/src/main/java/com/ordana/immersive_weathering/dynamicpack/ClientDynamicResourcesHandler.java
+++ common/src/main/java/com/ordana/immersive_weathering/dynamicpack/ClientDynamicResourcesHandler.java
@@ -143,26 +143,20 @@ public class ClientDynamicResourcesHandler extends DynClientResourcesGenerator {
 
             ModItems.BARK.forEach((woodType, bark) -> {
                 if (!woodType.isVanilla() || !PlatHelper.isDev()) {
-
                     String id = Utils.getID(bark).getPath();
-                    if(id.endsWith("scales")
-                       {
-                                            try {
-                        addSimilarJsonResource(manager,barkModel, "oak_bark", id);
-                    } catch (Exception ex) {
-                        getLogger().error("Failed to generate Bark item model for {} : {}", bark, ex);
+                    if(id.endsWith("scales")) {
+                        try {
+                            addSimilarJsonResource(manager, barkModel, "oak_bark", id);
+                        } catch (Exception ex) {
+                            getLogger().error("Failed to generate Bark item model for {} : {}", bark, ex);
+                        }
+                    } else {
+                        try {
+                            addSimilarJsonResource(manager, scaleModel, "crimson_scales", id);
+                        } catch (Exception ex) {
+                            getLogger().error("Failed to generate Scales item model for {} : {}", bark, ex);
+                        }
                     }
-                    }
-                    else
-                    {
-                                            try {
-                        addSimilarJsonResource(manager,scaleModel, "crimson_scales", id);
-                    } catch (Exception ex) {
-                        getLogger().error("Failed to generate Scales item model for {} : {}", bark, ex);
-                    }
-                    }
-
-
                 }
             });
         }
@@ -195,7 +189,7 @@ public class ClientDynamicResourcesHandler extends DynClientResourcesGenerator {
         });
 
         //bark textures
-        try (TextureImage template = TextureImage.open(manager, ImmersiveWeathering.res("item/bark_template"))) {
+        try (TextureImage template = TextureImage.open(manager, ImmersiveWeathering.res("item/bark_template")); TextureImage scalesTemplate = TextureImage.open(manager, ImmersiveWeathering.res("item/scales_template"))) {
 
             ModItems.BARK.forEach((type, bark) -> {
 
@@ -204,15 +198,20 @@ public class ClientDynamicResourcesHandler extends DynClientResourcesGenerator {
                 ResourceLocation textureRes = ImmersiveWeathering.res(
                         "item/" + Utils.getID(bark).getPath());
                 if (!alreadyHasTextureAtLocation(manager, textureRes)) {
-
+                    String id = Utils.getID(bark).getPath();
+                    TextureImage tempTemplate = template;
+                    if(id.endsWith("scales"))
+                    {
+                        tempTemplate = scalesTemplate;
+                    }
                     try (TextureImage logTexture = TextureImage.open(manager,
                             RPUtils.findFirstBlockTextureLocation(manager, type.log, s -> !s.contains("top")))) {
                         Palette palette = Palette.fromImage(logTexture);
                         //PaletteColor average = palette.calculateAverage();
                         palette.increaseDown();
                         PaletteColor dark = palette.getDarkest();
-                        assert template.imageWidth() <= logTexture.imageWidth() && template.imageHeight() <= logTexture.imageHeight();
-                        TextureImage newImage = template.makeCopy();
+                        assert tempTemplate.imageWidth() <= logTexture.imageWidth() && tempTemplate.imageHeight() <= logTexture.imageHeight();
+                        TextureImage newImage = tempTemplate.makeCopy();
                         var logImage = logTexture.getImage();
                         newImage.forEachFrame((i, x, y) -> {
                             var image = newImage.getImage();
@@ -242,7 +241,14 @@ public class ClientDynamicResourcesHandler extends DynClientResourcesGenerator {
     @Override
     public void addDynamicTranslations(AfterLanguageLoadEvent lang) {
         ModItems.BARK.forEach((type, bark) -> {
-            LangBuilder.addDynamicEntry(lang, "item.immersive_weathering.bark", type, bark);
+            String id = Utils.getID(bark).getPath();
+            if(id.endsWith("scales"))
+            {
+                LangBuilder.addDynamicEntry(lang, "item.immersive_weathering.scales", type, bark);
+            }
+            else{
+                LangBuilder.addDynamicEntry(lang, "item.immersive_weathering.bark", type, bark);
+            }
         });
         ModBlocks.LEAF_PILES.forEach((type, leaf) -> {
             LangBuilder.addDynamicEntry(lang, "block.immersive_weathering.leaf_pile", type, leaf);
